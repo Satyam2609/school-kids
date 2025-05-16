10d1524774bee4a32a9fa0da8e090c6828d567b7
Satyam Jain <satyam26092004@gmail.com>
2025-05-16 13:38:55 +0530

    Initial commit with responsive Navigation component

--- kidschool/src/Components/Navigation/Navigation.jsx
+++ kidschool/src/Components/Navigation/Navigation.jsx
@@ -4,49 +4,48 @@ import { NavLink } from 'react-router-dom'
 function Navigation() {
   return (
     <>
-      <div className="relative h-[13vh] w-full flex items-center">
+      <div className="relative flex items-center h-[13vh] w-full">
 
-        {/* Gray logo section */}
-        <div className="bg-gray-300 h-full w-[60%] flex items-center pl-4 md:pl-6 border-l-2 relative z-20">
+        {/* Gray section with logo */}
+        <div className="bg-gray-300 h-full md:w-[35%] w-[40%] flex items-center pl-6 border-l-2 z-20 relative">
           <img
             src="/assets/logo.avif"
             alt="Logo"
-            className="h-[6vh] md:h-[8vh] w-auto object-contain"
+            className="h-[8vh] w-auto object-contain"
           />
         </div>
 
-        {/* Purple navbar section */}
+        {/* Purple nav links section */}
         <div
-          className="bg-purple-600 h-full w-[50%] rounded-l-full flex items-center justify-around border-l-2 relative"
-          style={{
-            marginLeft: '-10%', // overlapping thoda adjust karne ke liye
-            zIndex: 10,
-          }}
+          className="bg-purple-600 h-full md:w-[65%] w-[60%] rounded-l-full flex items-center justify-center border-l-2 relative"
+          style={{ marginLeft: '-8%' }} // overlap thoda adjust karne ke liye
         >
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? 'text-yellow-400 font-semibold' : 'text-white'
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/Admission"
-            className={({ isActive }) =>
-              isActive ? 'text-yellow-400 font-semibold' : 'text-white'
-            }
-          >
-            Admissions
-          </NavLink>
-          <NavLink
-            to="/About us"
-            className={({ isActive }) =>
-              `${isActive ? 'text-yellow-400 font-semibold' : 'text-white'} whitespace-nowrap`
-            }
-          >
-            About us
-          </NavLink>
+          <div className="flex justify-around items-center w-full px-6">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? 'text-yellow-400 font-semibold' : 'text-white'
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/Admission"
+              className={({ isActive }) =>
+                isActive ? 'text-yellow-400 font-semibold' : 'text-white'
+              }
+            >
+              Admissions
+            </NavLink>
+            <NavLink
+              to="/About us"
+              className={({ isActive }) =>
+                `${isActive ? 'text-yellow-400 font-semibold' : 'text-white'} whitespace-nowrap`
+              }
+            >
+              About us
+            </NavLink>
+          </div>
         </div>
       </div>
     </>
