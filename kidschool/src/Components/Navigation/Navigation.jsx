bd6a9b782237cab2669a4ec697da57a7217b8ac1
Satyam Jain <satyam26092004@gmail.com>
2025-05-16 13:54:09 +0530

    Initial commit with responsive Navigation component

--- kidschool/src/Components/Navigation/Navigation.jsx
+++ kidschool/src/Components/Navigation/Navigation.jsx
@@ -4,27 +4,27 @@ import { NavLink } from 'react-router-dom'
 function Navigation() {
   return (
     <>
-      <div className="relative flex items-center h-[13vh] w-full">
+      <div className="relative flex items-center h-[5vh] md:h-[13vh] w-full">
 
         {/* Gray section with logo */}
-        <div className="bg-gray-300 h-full md:w-[35%] w-[40%] flex items-center pl-6 border-l-2 z-20 relative">
+        <div className="bg-gray-300 h-full md:w-[35%] w-[55%] flex items-center pl-4 md:pl-6 border-l-2 z-20 relative" style={{ zIndex: 30 }}>
           <img
             src="/assets/logo.avif"
             alt="Logo"
-            className="h-[8vh] w-auto object-contain"
+            className="h-[3.5vh] md:h-[8vh] w-auto object-contain"
           />
         </div>
 
         {/* Purple nav links section */}
         <div
-          className="bg-purple-600 h-full md:w-[65%] w-[60%] rounded-l-full flex items-center justify-center border-l-2 relative"
-          style={{ marginLeft: '-8%' }} // overlap thoda adjust karne ke liye
+          className="bg-purple-600 h-full md:w-[70%] w-[80%] flex items-center justify-center border-l-2 relative"
+          
         >
-          <div className="flex justify-around items-center w-full px-6">
+          <div className="flex justify-around items-center w-full px-4 md:px-6">
             <NavLink
               to="/"
               className={({ isActive }) =>
-                isActive ? 'text-yellow-400 font-semibold' : 'text-white'
+                isActive ? 'text-yellow-400 font-semibold text-xs md:text-base' : 'text-white text-xs md:text-base'
               }
             >
               Home
@@ -32,7 +32,7 @@ function Navigation() {
             <NavLink
               to="/Admission"
               className={({ isActive }) =>
-                isActive ? 'text-yellow-400 font-semibold' : 'text-white'
+                isActive ? 'text-yellow-400 font-semibold text-xs md:text-base' : 'text-white text-xs md:text-base'
               }
             >
               Admissions
@@ -40,7 +40,7 @@ function Navigation() {
             <NavLink
               to="/About us"
               className={({ isActive }) =>
-                `${isActive ? 'text-yellow-400 font-semibold' : 'text-white'} whitespace-nowrap`
+                `${isActive ? 'text-yellow-400 font-semibold' : 'text-white'} whitespace-nowrap text-xs md:text-base`
               }
             >
               About us
