59a70b72dbd9a4d61c94c4d0a9354a464e6e6cb2
Satyam Jain <satyam26092004@gmail.com>
2025-05-16 13:35:34 +0530

    Initial commit with responsive Navigation component

--- kidschool/src/Components/Navigation/Navigation.jsx
+++ kidschool/src/Components/Navigation/Navigation.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navigation() {
   return (
     <>
       <div className="relative h-[13vh] w-full flex items-center">
 
-        {/* Gray bg + logo */}
-        <div className="bg-gray-300 h-full w-[60%] flex items-center pl-4 md:pl-6 border-l-2">
+        {/* Gray logo section */}
+        <div className="bg-gray-300 h-full w-[60%] flex items-center pl-4 md:pl-6 border-l-2 relative z-20">
           <img
             src="/assets/logo.avif"
             alt="Logo"
@@ -15,32 +15,30 @@ function Navigation() {
           />
         </div>
 
-        {/* Purple navbar */}
-        <div className="bg-purple-600 h-full w-[40%] md:w-[40%] rounded-l-full flex items-center justify-around border-l-2">
-
-          {/* Nav links */}
+        {/* Purple navbar section */}
+        <div
+          className="bg-purple-600 h-full w-[50%] rounded-l-full flex items-center justify-around border-l-2 relative"
+          style={{
+            marginLeft: '-10%', // overlapping thoda adjust karne ke liye
+            zIndex: 10,
+          }}
+        >
           <NavLink
             to="/"
             className={({ isActive }) =>
-              isActive
-                ? 'text-yellow-400 font-semibold'
-                : 'text-white'
+              isActive ? 'text-yellow-400 font-semibold' : 'text-white'
             }
           >
             Home
           </NavLink>
-
           <NavLink
             to="/Admission"
             className={({ isActive }) =>
-              isActive
-                ? 'text-yellow-400 font-semibold'
-                : 'text-white'
+              isActive ? 'text-yellow-400 font-semibold' : 'text-white'
             }
           >
             Admissions
           </NavLink>
-
           <NavLink
             to="/About us"
             className={({ isActive }) =>
@@ -52,7 +50,7 @@ function Navigation() {
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Navigation;
+export default Navigation
