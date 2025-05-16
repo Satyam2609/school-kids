f95f41759dfb4b6a7e82b4abf50cb6f478b8c12c
Satyam Jain <satyam26092004@gmail.com>
2025-05-16 13:14:12 +0530

    Initial commit with responsive Navigation component

--- kidschool/src/Components/Navigation/Navigation.jsx
+++ kidschool/src/Components/Navigation/Navigation.jsx
@@ -4,20 +4,28 @@ import { NavLink } from 'react-router-dom'
 function Navigation() {
   return (
     <>   
-      {/* Logo Section */}
-      <div className='bg-gray-300 h-[13vh] w-full md:w-[70vw] flex justify-start items-center border-l-2'>
-        {/* Responsive Logo */}
+      {/* ✅ Desktop Logo Section */}
+      <div className='bg-gray-300 h-[13vh] w-full md:w-[70vw] justify-start items-center border-l-2 hidden md:flex'>
         <img 
           src='/assets/logo.avif' 
-          className='h-[6vh] w-auto ml-4 md:h-[8vh] md:ml-[6vw] object-contain' 
+          className='h-[8vh] w-auto ml-[6vw] object-contain' 
           alt="Logo" 
         />
       </div>
 
-      {/* Navigation Links Section */}
-      <div className='flex justify-end relative bottom-[13vh]'>
-        <div className='bg-purple-600 h-[13vh] w-full md:w-[70vw] rounded-l-full text-center border-l-2'>
-          <div className='relative top-[4vh] mx-auto flex justify-around items-center w-[90%] md:w-[50%]'>
+      {/* ✅ Navigation + Mobile Logo */}
+      <div className='flex justify-end relative md:bottom-[13vh]'>
+        <div className='bg-purple-600 h-[13vh] w-full md:w-[70vw] rounded-l-full text-center border-l-2 flex items-center justify-center relative'>
+
+          {/* ✅ Mobile Logo (visible only on small screens) */}
+          <img 
+            src='/assets/logo.avif' 
+            className='h-[5vh] w-auto absolute left-4 top-4 object-contain md:hidden' 
+            alt="Logo" 
+          />
+
+          {/* ✅ Navigation Links */}
+          <div className='mx-auto flex justify-around items-center w-[90%] md:w-[50%]'>
             <div>
               <NavLink to='/' className={({isActive}) => isActive ? 'text-yellow-400 font-semibold' : 'text-white'}>
                 Home
