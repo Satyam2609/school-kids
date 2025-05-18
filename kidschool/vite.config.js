bd5043441f1a020849439d35a104f96969a4b1aa
Satyam Jain <satyam26092004@gmail.com>
2025-05-18 15:36:23 +0530

    Initial commit: all  with homepage UI

--- kidschool/vite.config.js
+++ kidschool/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       'react-alice-carousel': 'react-alice-carousel',
+      '@': path.resolve(__dirname, './src') // optional shortcut for imports
     }
+  },
+  build: {
+    outDir: 'dist' // Ensure this matches your vercel.json if custom
+  },
+  // 👇👇 Add this part
+  server: {
+    historyApiFallback: true
   }
 })
