48c9b7398280edded085fa01a69a8a9e3f2a29d8
Satyam Jain <satyam26092004@gmail.com>
2025-05-23 15:29:46 +0530

    Initial commit:home  with home  UI

--- kidschool/src/Components/Admision/AdmisionData.jsx
+++ kidschool/src/Components/Admision/AdmisionData.jsx
@@ -1,10 +1,10 @@
 import image1 from '../../../public/assets/butterfly.png'
-import image2 from '../../../public/assets/AdmisionImage.png'
+
 
 const data = [
     {
         image : image1,
-        image1: image2
+        
     }
    
 ]
